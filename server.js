e812e9af5c7fa581c737d5072e8d9414236e4a1d
JillMOry <oryjill@gmail.com>
2022-02-08 20:20:36 -0600

    adding digital ocean deploy button

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ let express = require('express');
 
 
 let app = express();
-// app.listen(8080);
-app.listen(3001);
+app.listen(8080);
+// app.listen(3001);
 
 
 function multiTable(id){
     let table = [];
+    if(id > 45){
+        return "Select a number less than 45"
+    }
+    if(id < 1){
+        return "Select a number greater than zero"
+    }else{
     for(let r = 1; r <= id; r += 1){
         let row = [];
         for(let c = 1; c <= id; c += 1){
             row.push(r * c);
         }
         table.push(row);
-
+    }
     }
 //    console.table(table);
    return table;
